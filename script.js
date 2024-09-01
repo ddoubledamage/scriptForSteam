34cef170560a8631d08fc9c63fccd48bcea28fa1
Hatsi <rageofnature322@gmail.com>
2024-09-01 04:37:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let tdElements = document.querySelectorAll('td.wht_total');
+let totalSum = 0;
+tdElements.forEach(td => {
+    let div = td.querySelector('div');
+    if (div) {
+        let amountText = div.innerText.trim();
+        let amount = parseFloat(amountText.replace(' руб.', '').replace(/\s/g, ''));
+        totalSum += amount;
+    }
+});
+
+console.log(`Общая сумма: ${totalSum} руб.`);
